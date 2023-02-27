0732159d8ccf4833bed4ab54e2ef912ae40cd9aa
CHIKU BORAH <chiku.borah@w3villa.com>
2023-02-27 15:31:15 +0530

    done

---
+++ script.js
@@ -0,0 +1,129 @@
+$(document).ready(function(){
+$('#owltwo.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        },
+    
+        1220:{
+            items:4
+        },
+    
+        1460:{
+            items:5
+        },
+    }
+    
+    })
+    
+    $('#owlone.owl-carousel').owlCarousel({
+    // loop:true,
+    margin:0,
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        },
+    
+        1220:{
+            items:6
+        },
+    
+        1460:{
+            items:9
+        },
+    }
+    
+    });
+    $('#owlthree.owl-carousel').owlCarousel({
+    // loop:true,
+    margin:0,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:2
+        },
+    
+        1220:{
+            items:3
+        },
+    
+        1460:{
+            items:3
+        },
+    }
+    
+    });
+    $('#owlnine.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        },
+    
+        1220:{
+            items:3
+        },
+    
+        1460:{
+            items:4
+        },
+    }
+    
+    });
+    $('#owlfour.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1084:{
+            items:2
+        },
+    
+        1220:{
+            items:2
+        },
+        
+    
+        1400:{
+            items:3
+        },
+    }
+    
+    });
+    });
