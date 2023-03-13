18a988db4def2fe1c9b4f19f7f1280c41602c5b1
CHIKU BORAH <chiku.borah@w3villa.com>
2023-03-13 18:22:34 +0530

    done

--- script.js
+++ script.js
@@ -3,6 +3,58 @@ $('#owltwo.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
     nav:false,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        },
+    
+        1220:{
+            items:4
+        },
+    
+        1460:{
+            items:5
+        },
+    }
+    
+    })
+$('#owlSecond.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        },
+    
+        1220:{
+            items:1
+        },
+    
+        1460:{
+            items:1
+        },
+    }
+    
+    })
+$('#owlFirst.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
     responsive:{
         0:{
             items:1
@@ -100,7 +152,7 @@ $('#owltwo.owl-carousel').owlCarousel({
     }
     
     });
-    $('#owlfour.owl-carousel').owlCarousel({
+    $('#owlfour').owlCarousel({
     loop:true,
     margin:0,
     nav:false,
@@ -132,18 +184,27 @@ function tapButtonBuy(para){
     let boxC= document.getElementsByClassName('boxc1')[0];
     boxC.classList.remove("boxc1");
     para.classList.add("boxc1");
+    let a =para.innerHTML.trim();
+    // console.log(a);
+    readData3(a);
 
 }
 function tapButtonFeature(para){
     let boxC= document.getElementsByClassName('boxc1-feature')[0];
     boxC.classList.remove("boxc1-feature");
     para.classList.add("boxc1-feature");
+    let a =para.innerHTML.trim();
+    // console.log(a);
+    readData2(a);
+
 
 }
 function tapButtonBlog(para){
     let boxC= document.getElementsByClassName('boxc1-blog')[0];
     boxC.classList.remove("boxc1-blog");
     para.classList.add("boxc1-blog");
+    let a =para.innerHTML.trim();
+    readDataBlog(a);
 
 }
 
